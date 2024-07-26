e194b091fffc340e314b22bbd3ffb6cc85a74ad2
Tati Guzman <tati.guzman96@gmail.com>
2024-07-25 20:43:49 -0700

    Changes added ingredient via DOM to one from optional list

--- recipepage/script.js
+++ recipepage/script.js
@@ -1,3 +1,5 @@
-var newListItem = document.createElement("li");
-newListItem.textContent = "Adobo";
-document.querySelector('ul').appendChild(newListItem);
+//Adding One More Element via DOM
+const newListItem = document.createElement("li");
+const optionalList = document.getElementById("optionalList");
+newListItem.textContent = "Cilantro for garnish";
+optionalList.appendChild(newListItem);
