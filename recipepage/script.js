6a8137d74f0724fcdbd9b2921e3a3403dc7cd461
Tati Guzman <tati.guzman96@gmail.com>
2024-07-25 21:13:59 -0700

    Adds button and styling for email address

--- recipepage/script.js
+++ recipepage/script.js
@@ -3,3 +3,20 @@ const newListItem = document.createElement("li");
 const optionalList = document.getElementById("optionalList");
 newListItem.textContent = "Cilantro for garnish";
 optionalList.appendChild(newListItem);
+
+//Feature 1: Email Recipe button with prompt to enter email address
+const emailBtn = document.createElement("button");
+emailBtn.type = "button";
+emailBtn.innerHTML = "Email me the recipe!";
+emailBtn.style.backgroundColor = "#8B0000";
+emailBtn.style.color = "white";
+
+const steps = document.getElementById("steps");
+steps.appendChild(emailBtn);
+
+//emailBtn.addEventListener("click", prompt("Please enter your email address."));
+
+
+//Feature 2: Header backgrounds change color when you hover over them
+
+//Feature 3: Hidden content - click on Recipe Prep and reveal box with message
