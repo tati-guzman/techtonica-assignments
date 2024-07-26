196e6a4e6a6cb16ed42fa5f14bd030d79c044588
Tati Guzman <tati.guzman96@gmail.com>
2024-07-25 22:18:35 -0700

    Changes header background color to orange after mouseover

--- recipepage/script.js
+++ recipepage/script.js
@@ -21,5 +21,21 @@ function getEmail() {
 emailBtn.addEventListener("click", getEmail);
 
 //Feature 2: Header backgrounds change color when you hover over them
+const headersArr = ["h1", "h2", "h3"];
+
+for (let i = 0; i < headersArr.length; i++) {
+    let header = document.getElementsByTagName(headersArr[i]);
+    for (let j = 0; j < header.length; j++) {
+        header[j].classList.add("header");
+    }
+}
+
+const allHeaders = document.getElementsByClassName("header");
+
+for (let i = 0; i < allHeaders.length; i++) {
+    allHeaders[i].addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "orange";
+    });
+}
 
 //Feature 3: Hidden content - click on Recipe Prep and reveal box with message
