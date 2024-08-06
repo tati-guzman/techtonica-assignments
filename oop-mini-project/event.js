1e917e2ec22e68884f3f4b13bbb8146eafa0b1ea
Tati Guzman <tati.guzman96@gmail.com>
2024-08-06 12:09:57 -0700

    Creates two more events (Step 5)

--- oop-mini-project/event.js
+++ oop-mini-project/event.js
@@ -9,8 +9,19 @@ class Event {
 
 //STEP 4: Create an object using the class
 const eventObj1 = new Event(
-    "Green Day Concert",
+    "Green Day",
     "2024 Tour: Performing the album 'Dookie' in its entirety"
 );
+
+//STEP 5: Create a few more objects with different values
+const eventObj2 = new Event(
+    "Blink-182",
+    "2023-2024 World Tour: A medley of their best set list so far"
+)
+
+const eventObj3 = new Event(
+    "Ed Sheeran",
+    "Mathematics Tour: his version of the Eras Tour that really should not have been a stadium tour"
+)
 
 
