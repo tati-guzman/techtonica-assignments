472ed12e9d95de47d4f31edd434f6797f4236a9b
Tati Guzman <tati.guzman96@gmail.com>
2024-08-06 12:05:08 -0700

    Creates first event using the class Event (Step 4)

--- oop-mini-project/event.js
+++ oop-mini-project/event.js
@@ -6,3 +6,11 @@ class Event {
         this.availableTickets = [];
     }
 }
+
+//STEP 4: Create an object using the class
+const eventObj1 = new Event(
+    "Green Day Concert",
+    "2024 Tour: Performing the album 'Dookie' in its entirety"
+);
+
+
