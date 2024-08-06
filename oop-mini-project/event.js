51e343773634add56a2fa69894ace960b214650c
Tati Guzman <tati.guzman96@gmail.com>
2024-08-06 12:01:16 -0700

    Creates event class (step 3)

---
+++ oop-mini-project/event.js
@@ -0,0 +1,8 @@
+//STEP 3: Create an Event class
+class Event {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+        this.availableTickets = [];
+    }
+}
