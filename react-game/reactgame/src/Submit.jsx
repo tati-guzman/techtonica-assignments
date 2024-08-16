18b631a6fbdf83b43072b8867f47ef3d0277abb4
Tati Guzman <tati.guzman96@gmail.com>
2024-08-15 21:17:11 -0700

    Styles new submit button

--- react-game/reactgame/src/Submit.jsx
+++ react-game/reactgame/src/Submit.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './App.jsx'
 
 const Submit = () => {
+    //Creates a variable to add styling within the component rather than in a stylesheet
+    const style = {
+        backgroundColor: "#f7fc81",
+        fontSize: "25px",
+        borderRadius: "50%",
+        width: "200px"
+    }
 
     return (
-        <button>Submit Guess</button>
+        <button style={style}>Submit Guess</button>
     )
 }
 
