df02155d3eb42d4e13354c637c5b6be18483fd7d
Tati Guzman <tati.guzman96@gmail.com>
2024-08-29 22:22:37 -0700

    Creates WeatherForm component

---
+++ weatherapp/client/src/WeatherForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.jsx'
+
+const WeatherForm = ({}) => {
+    
+    return (
+        <div>
+        
+        
+        </div>
+    )
+}
+
+export default WeatherForm
