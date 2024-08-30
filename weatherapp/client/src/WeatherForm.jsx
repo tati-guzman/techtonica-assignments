7f917f1d0d13e615eb76ea52ad4b3d870df627a7
Tati Guzman <tati.guzman96@gmail.com>
2024-08-30 09:25:30 -0700

    Cleans up comments in Weather Form

--- weatherapp/client/src/WeatherForm.jsx
+++ weatherapp/client/src/WeatherForm.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+//Import React functionality from react package
+import React, { useState } from 'react'
+//Import info from App
 import './App.jsx'
-import { useState } from 'react'
 
 
 const WeatherForm = ({setCity}) => {
+    
+    //Create a state for the text inputted into the search field
     const [inputText, setInputText] = useState("")
     
+    //When the user submits data, change the city value and clear the input field
     const handleSubmit = (event) => {
+        //Prevent the page from re-loading
         event.preventDefault();
+        
+        //Print info to console for troubleshooting
         console.log(event);
         console.log(event.target[0].value);
+        
+        //Update city value with setCity
         setCity(event.target[0].value);
+
+        //Clear the search field
         setInputText("")
       };
     
