24a89a269baf6309ccb32bf5c3bce8606b055247
Tati Guzman <tati.guzman96@gmail.com>
2024-08-29 20:55:29 -0700

    Configures api key in env file and adds get route for weather api data

--- weatherapp/server.js
+++ weatherapp/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(cors());
@@ -8,10 +10,20 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+dotenv.config();
+
 const PORT = 5002;
 
-app.get('/api',(req, res) => {
-    res.send('Hello World! This is working!');
+
+app.get('/api', async (req, res) => {
+    const apiKey = process.env.API_KEY;
+    const cityName = 'seattle'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
+    
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => res.send({data}))
+        .catch((err) => console.error({Details: err}));
 })
 
 //Sanity check to double check where the port is
