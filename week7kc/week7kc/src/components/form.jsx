5d74f2609b909f632e88c1737c194bc6f0fa8b09
Tati Guzman <tati.guzman96@gmail.com>
2024-08-30 18:15:20 -0700

    Fixes form to capture inputted data via useRef

--- week7kc/week7kc/src/components/form.jsx
+++ week7kc/week7kc/src/components/form.jsx
@@ -1,21 +1,30 @@
+//useRef is a React hook that allows you to store values similar to state but does not re-render the component!
 import React, { useRef } from "react";
 import "../App.css";
 
 const RegisterYourApplicationForm = ({onSubmit}) => {
 
     //All these variables are being created as refs for their respective question in the form
-    const userName = useRef();
-    const userEmail = useRef();
-    const userOrderNum = useRef();
-    const reasonForReturn = useRef();
+    const userName = useRef(null);
+    const userEmail = useRef(null);
+    const userOrderNum = useRef(null);
+    const reasonForReturn = useRef(null);
+
+    const handleChange = (event, ref) => {
+        ref.current = event.target.value;
+    };
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const allUserInfo = {
-        
+            name: userName.current,
+            email: userEmail.current,
+            orderNum: userOrderNum.current,
+            reason: reasonForReturn.current
         };
 
-    onSubmit();
+        onSubmit(allUserInfo);
   };
 
   return (
@@ -23,17 +32,21 @@ const RegisterYourApplicationForm = ({onSubmit}) => {
       
       <label>Name: *
         <input 
-        type="text"
-        required
-        placeholder="Name"
+            type="text"
+            required
+            placeholder="Name"
+            onChange={(event) => handleChange(event, userName)}
+            ref={userName}
         />
       </label>
 
       <label>Email: *
         <input 
-        type="email"
-        required
-        placeholder="Email"
+            type="email"
+            required
+            placeholder="Email"
+            onChange={(event) => handleChange(event, userEmail)}
+            ref={userEmail}
         />
       </label>
 
@@ -43,12 +56,19 @@ const RegisterYourApplicationForm = ({onSubmit}) => {
             required
             min="0"
             placeholder="Please Enter Ticket Number"
+            onChange={(event) => handleChange(event, userOrderNum)}
+            ref={userOrderNum}
         />
       </label>
 
       <label>Reason for Return: *
         <p>Please be specific in your reasons for returning your item</p> 
-        <textarea name="reason" required/>
+        <textarea
+            name="reason"
+            required
+            onChange={(event) => handleChange(event, reasonForReturn)}
+            ref={reasonForReturn}
+        />
       </label>
 
       <button type="submit">Submit</button>
