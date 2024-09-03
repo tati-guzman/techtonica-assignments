2556e6661942cac7829f63d4ddc529e22940f2ef
Tati Guzman <tati.guzman96@gmail.com>
2024-09-02 18:56:04 -0700

    Cleans up comments and files related to outside images

--- weatherapp/client/src/Weather.jsx
+++ weatherapp/client/src/Weather.jsx
@@ -2,40 +2,8 @@
 import React, { useState } from 'react'
 //Import info from App
 import './App.jsx'
-//Import weather icons from assets folder
-// import cloudy from './assets/cloudy.png'
-// import rainy from './assets/rainy.png'
-// import snowy from './assets/snowy.png'
-// import sunny from './assets/sunny.png'
-
 
 const Weather = ({weatherData}) => {
-    
-    // const forecastOptions = {"Clear": sunny, "Clouds": cloudy, "Rain": rainy, "Snow": snowy, "Haze": cloudy, "Mist": cloudy};
-    // const alt = {
-    //     sunny: "bright yellow sun with two flames on either side", 
-    //     cloudy:"four white bubbly clouds with part of a cartoon sun poking out behind the clouds", 
-    //     rainy: "one gray cloud with four blue raindrops falling out surrounded by four yellow lightning bolts",
-    //     snowy: "two gray clouds with several blue snowflakes"
-    // }
-
-    // const [forecast, setForecast] = useState(null);
-
-    //Log weather data passed into this component for debugging
-    // console.info(weatherData)
-    
-    // const [icon, setIcon] = useState("");
-    
-
-    //Update icon with setIcon //NEED TO FIGURE THIS OUT
-    console.info('weatherData', weatherData)
-    // if (weatherData && weatherData.weather.length > 0) {
-    //     const iconId = weatherData.weather[0].icon; 
-    //     setIcon(weatherData.weather[0].icon);
-    //     console.log(iconId);
-    // }
-
-    //  const iconURL = `http://openweathermap.org/img/w/${icon}.png`
 
     return (
         <div className="info">
