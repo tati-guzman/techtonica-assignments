83d7c42d2a8ee8b8d95034af02b4ecbd2b4b9107
Tati Guzman <tati.guzman96@gmail.com>
2024-09-11 15:08:50 -0700

    Resets configs to connect with database via hardcoded variables

--- eventonica/server/db/db-connection.js
+++ eventonica/server/db/db-connection.js
@@ -1,6 +1,25 @@
-const { Pool } = require('pg');
-const db = new Pool({
-    connectionString: process.env.DATABASE_URI
-  });
+//Import Pool class from pg package to use for connection pooling
+import pkg from 'pg';
 
-  module.exports = db;
+const { Pool } = pkg;
+
+//Create db instance of Pool class with secret configs
+const db = new Pool({
+    user: process.env.DB_USER,          
+    host: process.env.DB_HOST,          
+    database: process.env.DB_NAME,      
+    password: process.env.DB_PASSWORD,  
+    port: process.env.DB_PORT,    
+});
+
+//Export db object for server to import and use  
+// module.exports = db;
+export default db;
+
+//TO DELETE LATER:
+
+// const { Pool } = require('pg');
+
+// const db = new Pool({
+//     connectionString: process.env.DATABASE_URI
+//   });
