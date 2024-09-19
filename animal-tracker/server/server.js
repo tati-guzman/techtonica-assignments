98b793d42f6dc2130f072d18c445ca0ee16b7b47
Tati Guzman <tati.guzman96@gmail.com>
2024-09-19 12:52:18 -0700

    Adds pseudocoded comments for routes and removes template language

--- animal-tracker/server/server.js
+++ animal-tracker/server/server.js
@@ -1,90 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const path = require('path');
-const db = require('./db/db-connection.js');
+//Import frameworks for app and middleware
+import express from 'express';
+import cors from 'cors';
 
+//Import database connection
+import db from './db/db-connection.js';
 
+//Standard set-up operations for Express and Node
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8020;
 app.use(cors());
 app.use(express.json());
 
-// creates an endpoint for the route "/""
-app.get('/', (req, res) => {
-    res.json({ message: 'Hola, from My template ExpressJS with React-Vite' });
-});
+//Create Welcome Message for Root Directory
+//GET Request for '/'
 
-// create the get request for students in the endpoint '/api/students'
-app.get('/api/students', async (req, res) => {
-    try {
-        const { rows: students } = await db.query('SELECT * FROM students');
-        res.send(students);
-    } catch (e) {
-        return res.status(400).json({ e });
-    }
-});
+//Create GET Request for Sightings Table including nickname from Individuals Table (JOIN)
+//'/sightings'
 
-// create the POST request
-app.post('/api/students', async (req, res) => {
-    try {
-        const newStudent = {
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            iscurrent: req.body.iscurrent
-        };
-        //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
-        const result = await db.query(
-            'INSERT INTO students(firstname, lastname, is_current) VALUES($1, $2, $3) RETURNING *',
-            [newStudent.firstname, newStudent.lastname, newStudent.iscurrent],
-        );
-        console.log(result.rows[0]);
-        res.json(result.rows[0]);
+//Create GET Request for Individuals Table (all information from table)
+//'/individuals'
 
-    } catch (e) {
-        console.log(e);
-        return res.status(400).json({ e });
-    }
+//Create DELETE Request to remove a specified individual from Individuals Table
+//'/individuals/:animal_id'
 
-});
+//Create POST Request to create a new individual in the Individuals Table
+//'/individuals/:animal_id'
+//Reminder to RETURNING * to send back all rows to display in table
 
-// delete request for students
-app.delete('/api/students/:studentId', async (req, res) => {
-    try {
-        const studentId = req.params.studentId;
-        await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-        console.log("From the delete request-url", studentId);
-        res.status(200).end();
-    } catch (e) {
-        console.log(e);
-        return res.status(400).json({ e });
+//Create PUT Request to update individual in the Individuals Table
+//'/individuals/:animal_id'
+//Reminder to RETURNING * to show all rows in display
 
-    }
-});
+//Create POST Request to create new species record in Species Table
+//'/species'
 
-//A put request - Update a student 
-app.put('/api/students/:studentId', async (req, res) =>{
-    //console.log(req.params);
-    //This will be the id that I want to find in the DB - the student to be updated
-    const studentId = req.params.studentId
-    const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-    console.log("In the server from the url - the student id", studentId);
-    console.log("In the server, from the react - the student to be edited", updatedStudent);
-    // UPDATE students SET lastname = "something" WHERE id="16";
-    const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-    const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
-    try {
-      const updated = await db.query(query, values);
-      console.log(updated.rows[0]);
-      res.send(updated.rows[0]);
-  
-    }catch(e){
-      console.log(e);
-      return res.status(400).json({e})
-    }
-  })
+//Create POST Request to create new sighting record in Sightings Table
+//'/sightings'
 
-// console.log that your server is up and running
+//Print PORT location when active PORT is detected (server is running properly)
 app.listen(PORT, () => {
-    console.log(`Hola, Server listening on ${PORT}`);
+    console.log(`Welcome to Animal Tracker! Server is listening on PORT: ${PORT}`);
 });
