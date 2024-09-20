44d466a074874753edb978665ce499720729177f
Tati Guzman <tati.guzman96@gmail.com>
2024-09-20 11:33:41 -0700

    Add draft Landing functionality to toggle between healthy and unhealthy sightings

--- animal-tracker/client/src/components/Landing.jsx
+++ animal-tracker/client/src/components/Landing.jsx
@@ -1,5 +1,5 @@
 //Import necessary functionalities
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import Button from 'react-bootstrap/Button';
 import SightingsCard from './SightingsCard.jsx';
 
@@ -11,11 +11,16 @@ const Landing = () => {
     //State to capture Healthy Status filter
     const [showHealth, setShowHealth] = useState(false);
 
-    //onClick function for Healthy Sightings Button -> show only healthy sightings
-    //set showHealth to true
+    //Use Memoization to store the array of healthy sightings so long as the sightings state doesn't change
+    const healthySightings = useMemo(() => {
+        return sightings.filter((sighting) => 
+            sighting && typeof sighting.healthy === 'boolean' ? sighting.healthy : false);
+    }, [sightings]);
 
-    //
-
+    //onClick function for Healthy Sightings Button -> toggle between healthy sightings and all sightings by simply switching it to the opposite state
+    const toggleHealthy = () => {
+      setShowHealth(!showHealth);
+    }
     
     return (
         <>
@@ -26,8 +31,19 @@ const Landing = () => {
            {/* HEALTHY SIGHTING BUTTON: filter through fetched sightings for healthy = true */}
            {/* ALL SIGHTINGS BUTTON: onClick -> set showHealth to false */}
 
+           <Button onClick={toggleHealthy}>
+            {showHealth ?
+            "All Sightings" :
+            "Healthy Sightings Only"}
+           </Button>
+
            {/* Map out the fetched sightings that need to be shown into <SightingsCard /> */}
+           {showHealth ?
+           healthySightings.map((sighting) => <SightingsCard sighting={sighting} />) :
+           sightings.map((sighting) => <SightingsCard sighting={sighting} />)
+           }
         </>
     )
-
 }
+
+export default Landing
