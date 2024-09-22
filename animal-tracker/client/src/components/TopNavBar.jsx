b6ad06c1b3a64b4c67e671f8067a5458c360107c
Tati Guzman <tati.guzman96@gmail.com>
2024-09-22 10:30:25 -0700

    Fixed nav bar button functionalities - import button from bootstrap

--- animal-tracker/client/src/components/TopNavBar.jsx
+++ animal-tracker/client/src/components/TopNavBar.jsx
@@ -1,31 +1,29 @@
 //Import necessary styling 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 const TopNavBar = ({ setComponent }) => {
 
     return (
-        <>
-            <Navbar bg="light" sticky="top" className="d-flex justify-content-between">
-                <Container>
-                    {/* Brand redirects users to homepage, which in this case will also set component to landing */}
-                    <Navbar.Brand href="/" onClick={() => setComponent("landing")}>
-                        Animal Tracker 🔎
-                    </Navbar.Brand>
+        <Navbar bg="light" sticky="top" className="d-flex justify-content-between">
+            <Container>
+                <Navbar.Brand href="/" onClick={() => setComponent("landing")}>
+                    Animal Tracker 🔎
+                </Navbar.Brand>
 
-                    {/* The different buttons will determine which component is shown */}
-                    <Nav className="mx-auto">
-                        <Button onClick={() => setComponent("individuals")}>Individuals Info</Button>
-                        <Button onClick={() => setComponent("species")}>Add Species</Button>
-                        <Button onClick={() => setComponent("sightings")}>Add Sighting</Button>
-                    </Nav>
-          
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>Welcome, Super Scientist!</Navbar.Text>
-                    </Navbar.Collapse>
-                 </Container>
-            </Navbar>
-        </>
+                {/* The different buttons will determine which component is shown */}
+                <Navbar className="mx-auto">
+                    <Button onClick={() => setComponent("individuals")}>Individuals Info</Button>
+                    <Button onClick={() => setComponent("species")}>Add Species</Button>
+                    <Button onClick={() => setComponent("sightings")}>Add Sighting</Button>
+                </Navbar>
+        
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>Welcome, Super Scientist!</Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
