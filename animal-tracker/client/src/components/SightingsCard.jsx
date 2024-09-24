cf94f3be8768525e3acf16ef1830dfc29401c37b
Tati Guzman <tati.guzman96@gmail.com>
2024-09-23 19:44:52 -0700

    Corrects date display

--- animal-tracker/client/src/components/SightingsCard.jsx
+++ animal-tracker/client/src/components/SightingsCard.jsx
@@ -13,6 +13,15 @@ const SightingsCard = (sighting) => {
         hour12: false
     }) : 'TBD';
 
+    const correctDateTime = sighting.sighting.date_time ? new Date(sighting.sighting.date_time).toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    }) : 'TBD';
+
     return (
         <Card>
            <Card.Body>
@@ -20,7 +29,7 @@ const SightingsCard = (sighting) => {
                 <Card.Title>{sighting.sighting.animal_id}: {sighting.sighting.nickname}</Card.Title>
 
                 <Card.Text>
-                    Date & Time: {sighting.sighting.date_time}<br />
+                    Date & Time: {correctDateTime}<br />
                     Location: {sighting.sighting.location}<br />
                     Health Status: {sighting.sighting.healthy ? "Healthy" : sighting.sighting.healthy === false ? "Not Healthy" : "Unknown"}<br />
                     Sighter's Email: {sighting.sighting.sighter_email}<br />
