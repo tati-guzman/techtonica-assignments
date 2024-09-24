ea1c8bb74b1fcfbc747161e38232f9dbd97805d7
Tati Guzman <tati.guzman96@gmail.com>
2024-09-23 19:43:05 -0700

    cleans up comments in test

--- animal-tracker/client/src/components/SightingsForm.test.jsx
+++ animal-tracker/client/src/components/SightingsForm.test.jsx
@@ -1,18 +1,12 @@
-import React from 'react'; // This is essential for JSX
-
 //Import functionality from testing libraries
 import { render, screen, fireEvent } from '@testing-library/react'
 import { expect, test, vi } from 'vitest';
 
 //Import components
-// import Landing from "./Landing.jsx"
 import SightingsForm from './SightingsForm.jsx';
 
-
 //TEST ONE: Does the "Clear Form" button clear the fields?
-console.log("test file");
 test("clears fields correctly", () => {
-    console.log("will render");
     render(<SightingsForm />);
 
     const inputLocation = screen.getByPlaceholderText('Any location information is accepted!');
