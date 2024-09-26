17f67fb7d309d305889aad2750c4adeb2834ed2e
Tati Guzman <tati.guzman96@gmail.com>
2024-09-25 21:13:01 -0700

    Adds detailed comments and clarifications

--- quiz-app/client/src/components/UserSelection.jsx
+++ quiz-app/client/src/components/UserSelection.jsx
@@ -3,24 +3,40 @@ import React, { useState } from 'react';
 
 const UserSelection = ({ playerInfo, onStart, setCurrentPlayer }) => {
     
+    //State to track the username picked from the drop down (including if it's "Add New User")
     const [selectedUsername, setSelectedUsername] = useState("");
+
+    //State to track the new user name that is being submitted
     const [newUsername, setNewUsername] = useState("");
+
+    //State to track whether or not there will be a new submission -> logic to show added input field
     const [addingNewUser, setAddingNewUser] = useState(false);
 
+    //Function to update states accordingly based on what the user clicks from the drop down
     const handleChosenUsername = (event) => {
+        //Holds the selected option in the selectedUsername state
         setSelectedUsername(event.target.value);
+
+        //If the selected user name value is "new user option" then set addingNewUser to true to show the other input field
         setAddingNewUser(event.target.value === "new user option")
     }
 
     const handleNewUsername = (event) => {
+        //As the user inputs the new user name, store the answer for submission
         setNewUsername(event.target.value);
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        
+        //If there is a new user, use the newUsername state. Otherwise, use the selectedUsername from the dropdown
         const player = addingNewUser ? newUsername : selectedUsername;
-        //NOTE: Missing error handling for users with the same name
+        //NOTE: Missing error handling for users with the same name!!!
+        
+        //Store the selected username to use when the game is over
         setCurrentPlayer(player);
+
+        //Start the game with the same functionality as the "ready to rumble" button
         onStart();
     }
     
