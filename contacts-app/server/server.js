1742c309034bfffa44bc906f0a0011af2e412182
Tati Guzman <tati.guzman96@gmail.com>
2024-09-26 18:10:53 -0700

    Creates successful GET route for contact info and recent status

--- contacts-app/server/server.js
+++ contacts-app/server/server.js
@@ -11,6 +11,40 @@ const PORT = process.env.PORT || 8020;
 app.use(cors());
 app.use(express.json());
 
+//GET Route to pull all contacts and "recents" data
+//'/contacts'
+app.get('/contacts', async (req, res) => {
+    try {
+        const queryStatement = 'SELECT contacts.user_id, name, phone, email, birthday_notes, recent FROM contacts LEFT JOIN recents ON contacts.user_id = recents.user_id';
+        const contactInfo = await db.query(queryStatement);
+        res.json(contactInfo.rows);
+    } catch (error) {
+        res.status(500).json({ error: "Could not pull contact info", details: error });
+    }
+});
+
+//POST Route to create a new contact
+//'/contacts/'
+
+
+//POST Route to create new reminders
+//'/reminders/'
+
+//POST Route to create new recent status
+//'/recents/'
+
+//PUT Route to update existing contact
+//'/contacts/:user_id'
+
+//PUT Route to update reminders
+//'/contacts/:reminder_id'
+
+//PUT Route to update recent status
+//'/contacts/:user_id'
+
+//DELETE Route to delete a contact
+//'/contacts/'
+
 //Print PORT location when active PORT is detected (server is running properly)
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`);
