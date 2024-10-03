d6cc7ddf483c4b89cfdbfa85f5b0d65dfd0bd81d
Tati Guzman <tati.guzman96@gmail.com>
2024-10-03 09:31:57 -0700

    Creates successful test to check for rendering of both nav bar buttons

--- blog-app/client/src/__tests__/NavBar.test.jsx
+++ blog-app/client/src/__tests__/NavBar.test.jsx
@@ -1,7 +1,21 @@
 //Import all testing dependencies needed
 import '@testing-library/jest-dom';
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import { vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 //Import component being tested
 import NavBar from '../components/NavBar.jsx';
+
+describe('Navigation Bar', () => {
+
+    test('Nav Bar contains both a Posts button and Create a Form button', () => {
+        render(<NavBar />);
+
+        //Check for rendering of the "View All Posts" button
+        expect(screen.getByRole('button', { name: /view all posts/i })).toBeInTheDocument();
+
+        //Check for rendering of the "Create a Post" button
+        expect(screen.getByRole('button', { name: /create a post/i })).toBeInTheDocument();
+
+    })
+
+})
