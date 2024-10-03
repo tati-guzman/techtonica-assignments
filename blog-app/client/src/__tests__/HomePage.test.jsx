6fdcba914de449ce41f1a190e6181a1b9843dd77
Tati Guzman <tati.guzman96@gmail.com>
2024-10-03 05:47:22 -0700

    Creates templates for each component with the correct component imported and all required dependencies

---
+++ blog-app/client/src/__tests__/HomePage.test.jsx
@@ -0,0 +1,7 @@
+//Import all testing dependencies needed
+import '@testing-library/jest-dom';
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import { vi } from 'vitest';
+
+//Import component being tested
+import HomePage from '../components/HomePage.jsx';
---
+++ blog-app/client/src/__tests__/NavBar.test.jsx
@@ -0,0 +1,7 @@
+//Import all testing dependencies needed
+import '@testing-library/jest-dom';
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import { vi } from 'vitest';
+
+//Import component being tested
+import NavBar from '../components/NavBar.jsx';
