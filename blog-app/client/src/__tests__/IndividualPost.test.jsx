7a8060b16b2a3252f2561226af777353e318515d
Tati Guzman <tati.guzman96@gmail.com>
2024-10-03 10:12:49 -0700

    Creates successful test to check for that dates are being corrected and parsed appropriately so they render as Month Day, Year

--- blog-app/client/src/__tests__/IndividualPost.test.jsx
+++ blog-app/client/src/__tests__/IndividualPost.test.jsx
@@ -1,7 +1,21 @@
 //Import all testing dependencies needed
 import '@testing-library/jest-dom';
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import { vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 //Import component being tested
 import IndividualPost from '../components/IndividualPost.jsx';
+
+describe('Individual Post', () => {
+    //Set up mock post to use in rendering
+    const selectedPost = {
+        "title": "Testing out the Daisy blog",
+        "content": "This has been a crazy experience! I got to work on starting at pictures of my dog all day.",
+        "created_at": "2023-10-01T12:00:00Z"
+    }
+
+    test('Date is being parsed and rendered as Month Day, Year', () => {
+        render(<IndividualPost selectedPost={selectedPost} />);
+
+        expect(screen.getByRole('heading', { level: 4 })).toHaveTextContent('October 1, 2023');
+    })
+})
