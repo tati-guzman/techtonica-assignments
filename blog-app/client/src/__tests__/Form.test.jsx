0a61e96da954f32478dfef9b1057ec35b8634b2a
Tati Guzman <tati.guzman96@gmail.com>
2024-10-03 10:25:01 -0700

    Adds successful test to check the the Cancel button clears all input

--- blog-app/client/src/__tests__/Form.test.jsx
+++ blog-app/client/src/__tests__/Form.test.jsx
@@ -1,7 +1,25 @@
 //Import all testing dependencies needed
 import '@testing-library/jest-dom';
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import { vi } from 'vitest';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 
 //Import component being tested
 import Form from '../components/Form.jsx';
+
+describe('Form', () => {
+    
+    test('cancel button will clear form', () => {
+        render(<Form />);
+
+        //Mock input text into title and content area
+        act(() => {
+            fireEvent.change(screen.getByLabelText(/title/i), { target: {value : 'Another Mock Post' }});
+            fireEvent.change(screen.getByLabelText(/content/i), { target: { value: 'Mock content' }});
+
+            //Click the cancel button to clear the form
+            fireEvent.click(screen.getByRole('button', { name: /cancel/i }));
+        });
+
+        expect(screen.getByLabelText(/title/i).value).toBe('');
+        expect(screen.getByLabelText(/content/i).value).toBe('');
+    })
+})
